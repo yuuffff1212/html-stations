fa64ca559a2622469a6ae50c3a40e8cf1e03a8e5
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-09-28 22:02:12 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,22 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    { id: 1, first_name: "優", family_name: "大木", affilication: "TechTrain", is_student: false },
+    { id: 2, first_name: "太郎", family_name: "山田", affilication: "HogeHoge大学", is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise(resolve => {
+      setTimeout(() => {
+          const result = userList.map((u) => ({
+            ...u, full_name:  `${u.family_name} ${u.first_name}`
+          }))
+        resolve(result)},3000
+      );
+    }
+  );
 }
 
